b48c325b59c6fb28716a65eb7bcb6756cb38aaaf
William <William.f.Monahan@gmail.com>
2018-04-28 12:38:49 -0400

    'Initial'

---
+++ EFRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext dBContext;
+
+        public EFRepository(DbContext dbContext)
+        {
+            dBContext = dbContext;
+        }
+
+        public void Add(TEntity entity)
+        {
+            dBContext.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            dBContext.Set<TEntity>().AddRange(entities);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return dBContext.Set<TEntity>().Where(predicate);
+        }
+
+        public TEntity Get(long id)
+        {
+            return dBContext.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return dBContext.Set<TEntity>().ToList();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            dBContext.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            dBContext.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public string ConnectionString => "Data Source=PMD1SQLDW1P;Initial Catalog=Firazyr;Integrated Security=True;MultipleActiveResultSets=True";
+
+        public Repository()
+        {
+        }
+
+        public void Add(TEntity entity)
+        {
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+        }
+
+        public abstract IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        public abstract TEntity Get(long id);
+
+        public abstract IEnumerable<TEntity> GetAll();
+
+        public void Remove(TEntity entity)
+        {
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+        }
+    }
+}
